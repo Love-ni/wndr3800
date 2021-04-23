08e9c9ebf5c800d3b8451d3c4f0a0991e1542e57
lianghong-Y <55963334+lianghong-Y@users.noreply.github.com>
2021-04-23 13:38:41 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -22,7 +22,8 @@
 
 sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git kenzok8 https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git destan19 https://github.com/destan19/OpenAppFilter' feeds.conf.default
 
 #
 ./scripts/feeds update -a && ./scripts/feeds install -a
